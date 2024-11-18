fc65ee16476a2de969058e47e7bce2aaacb2703b
sysnote8main <sysnote8main@gmail.com>
2024-11-18 23:28:16 +0900

    [Can't-Build] Add packet codec on some packets

--- src/main/java/com/fusionflux/gravity_api/util/packet/InvertGravityPacket.java
+++ src/main/java/com/fusionflux/gravity_api/util/packet/InvertGravityPacket.java
@@ -3,41 +3,43 @@ package com.fusionflux.gravity_api.util.packet;
 import com.fusionflux.gravity_api.api.RotationParameters;
 import com.fusionflux.gravity_api.util.GravityComponent;
 import com.fusionflux.gravity_api.util.NetworkUtil;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.codec.PacketCodecs;
 
-public class InvertGravityPacket extends GravityPacket{
+public class InvertGravityPacket extends GravityPacket implements CilentboundAndServerboundPacketPayload {
+    public static final PacketCodec<PacketByteBuf, InvertGravityPacket> PACKET_CODEC = PacketCodec.tuple(
+            PacketCodecs.INT, p -> p.entityId,
+            PacketCodecs.BOOL, p -> p.inverted,
+            RotationParameters.PACKET_CODEC, p -> p.rotationParameters,
+            PacketCodecs.BOOL, p -> p.initialGravity,
+            InvertGravityPacket::new
+    );
+
     public final boolean inverted;
     public final RotationParameters rotationParameters;
     public final boolean initialGravity;
 
-    public InvertGravityPacket(boolean _inverted, RotationParameters _rotationParameters, boolean _initialGravity){
+    public InvertGravityPacket(int _entityId, boolean _inverted, RotationParameters _rotationParameters, boolean _initialGravity){
+        entityId = _entityId;
         inverted = _inverted;
         rotationParameters = _rotationParameters;
         initialGravity = _initialGravity;
     }
 
-    public InvertGravityPacket(PacketByteBuf buf) {
-        this(
-            buf.readBoolean(),
-            NetworkUtil.readRotationParameters(buf),
-            buf.readBoolean()
-        );
-    }
-
     @Override
-    public void write(PacketByteBuf buf) {
-        buf.writeBoolean(inverted);
-        NetworkUtil.writeRotationParameters(buf, rotationParameters);
-        buf.writeBoolean(initialGravity);
-    }
-
-    @Override
-    public void run(GravityComponent gc) {
+    public void handleBoth(PlayerEntity player, GravityComponent gc) {
         gc.invertGravity(inverted, rotationParameters, initialGravity);
     }
 
     @Override
     public RotationParameters getRotationParameters() {
         return rotationParameters;
+    }
+
+    @Override
+    public PacketTypeProvider getTypeProvider() {
+        return GravityPackets.INVERT_GRAVITY_PACKET;
     }
 }
--- src/main/java/com/fusionflux/gravity_api/util/packet/UpdateGravityPacket.java
+++ src/main/java/com/fusionflux/gravity_api/util/packet/UpdateGravityPacket.java
@@ -4,37 +4,39 @@ import com.fusionflux.gravity_api.api.RotationParameters;
 import com.fusionflux.gravity_api.util.Gravity;
 import com.fusionflux.gravity_api.util.GravityComponent;
 import com.fusionflux.gravity_api.util.NetworkUtil;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.codec.PacketCodecs;
 
-public class UpdateGravityPacket extends GravityPacket{
+public class UpdateGravityPacket extends GravityPacket implements CilentboundAndServerboundPacketPayload{
+    public static final PacketCodec<PacketByteBuf, UpdateGravityPacket> PACKET_CODEC = PacketCodec.tuple(
+            PacketCodecs.INT, p -> p.entityId,
+            Gravity.PACKET_CODEC, p -> p.gravity,
+            PacketCodecs.BOOL, p -> p.initialGravity,
+            UpdateGravityPacket::new
+    );
     public final Gravity gravity;
     public final boolean initialGravity;
 
-    public UpdateGravityPacket(Gravity _gravity, boolean _initialGravity){
+    public UpdateGravityPacket(int _entityId, Gravity _gravity, boolean _initialGravity) {
+        entityId = _entityId;
         gravity =  _gravity;
         initialGravity = _initialGravity;
     }
 
-    public UpdateGravityPacket(PacketByteBuf buf) {
-        this(
-            NetworkUtil.readGravity(buf),
-            buf.readBoolean()
-        );
-    }
-
     @Override
-    public void write(PacketByteBuf buf) {
-        NetworkUtil.writeGravity(buf, gravity);
-        buf.writeBoolean(initialGravity);
-    }
-
-    @Override
-    public void run(GravityComponent gc) {
+    public void handleBoth(PlayerEntity player, GravityComponent gc) {
         gc.addGravity(gravity, initialGravity);
     }
 
     @Override
     public RotationParameters getRotationParameters() {
         return gravity.rotationParameters();
+    }
+
+    @Override
+    public PacketTypeProvider getTypeProvider() {
+        return GravityPackets.UPDATE_GRAVITY_PACKET;
     }
 }
