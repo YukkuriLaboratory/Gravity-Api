11eb1cc2897a9279f8c491a827a47fcf4d288bc3
sysnote8main <sysnote8main@gmail.com>
2024-11-18 23:30:00 +0900

    [Can't-Build] Add GravityPacketSender.java for preparing new packet system

---
+++ src/main/java/com/fusionflux/gravity_api/util/packet/GravityPacketSender.java
@@ -0,0 +1,80 @@
+package com.fusionflux.gravity_api.util.packet;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.entity.Entity;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.payload.CustomPayload;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerChunkManager;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3i;
+
+public class GravityPacketSender {
+    @Environment(EnvType.CLIENT)
+    public static void sendToServer(CustomPayload payload) {
+        ClientPlayNetworking.send(payload);
+    }
+
+    public static void sendToClient(ServerPlayerEntity player, CustomPayload payload) {
+        ServerPlayNetworking.send(player, payload);
+    }
+
+    public static void sendToClients(Iterable<ServerPlayerEntity> players, CustomPayload payload) {
+        for (ServerPlayerEntity player : players) {
+            sendToClient(player, payload);
+        }
+    }
+
+    public static void sendToAllClients(MinecraftServer server, CustomPayload payload) {
+        Packet<?> packet = ServerPlayNetworking.createS2CPacket(payload);
+        server.getPlayerManager().sendToAll(packet);
+    }
+
+    public static void sendToAllExceptSelf(MinecraftServer server, ServerPlayerEntity self, CustomPayload payload) {
+        for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
+            if (player != self) {
+                sendToClient(player, payload);
+            }
+        }
+    }
+
+    public static void sendToClientsTrackingAndSelf(Entity entity, CustomPayload payload) {
+        Packet<?> packet = ServerPlayNetworking.createS2CPacket(payload);
+        if (entity.getWorld().getChunkManager() instanceof ServerChunkManager chunkCache) {
+            chunkCache.sendToNearbyPlayers(entity, packet);
+        } else {
+            throw new IllegalStateException("Cannot send clientbound payloads on the client");
+        }
+    }
+
+    public static void sendToClientsTrackingEntity(Entity entity, CustomPayload payload) {
+        Packet<?> packet = ServerPlayNetworking.createS2CPacket(payload);
+        if (entity.getWorld().getChunkManager() instanceof ServerChunkManager chunkCache) {
+            chunkCache.sendToOtherNearbyPlayers(entity, packet);
+        } else {
+            throw new IllegalStateException("Cannot send clientbound payloads on the client");
+        }
+    }
+
+    public static void sendToClientsTrackingChunk(ServerWorld ServerWorld, ChunkPos chunk, CustomPayload payload) {
+        for (ServerPlayerEntity player : ServerWorld.getChunkManager().delegate.getPlayersWatchingChunk(chunk, false)) {
+            sendToClient(player, payload);
+        }
+    }
+
+    public static void sendToClientsAround(ServerWorld ServerWorld, Vec3d pos, double radius, CustomPayload payload) {
+        Packet<?> packet = ServerPlayNetworking.createS2CPacket(payload);
+        ServerWorld.getServer().getPlayerManager().sendToAround(null, pos.getX(), pos.getY(), pos.getZ(), radius, ServerWorld.getRegistryKey(), packet);
+    }
+
+    public static void sendToClientsAround(ServerWorld ServerWorld, Vec3i pos, double radius, CustomPayload payload) {
+        sendToClientsAround(ServerWorld, new Vec3d(pos.getX(), pos.getY(), pos.getZ()), radius, payload);
+    }
+
+}
---
+++ src/main/java/com/fusionflux/gravity_api/util/packet/GravityPackets.java
@@ -0,0 +1,62 @@
+package com.fusionflux.gravity_api.util.packet;
+
+
+import com.fusionflux.gravity_api.GravityChangerMod;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.packet.payload.CustomPayload;
+
+import java.util.Locale;
+
+public enum GravityPackets implements BasePacketPayload.PacketTypeProvider {
+    // Client to Server & Server to Client
+    DEFAULT_GRAVITY_PACKET(DefaultGravityPacket.class, DefaultGravityPacket.PACKET_CODEC),
+    DEFAULT_GRAVITY_STRENGTH_PACKET(DefaultGravityStrengthPacket.class, DefaultGravityStrengthPacket.PACKET_CODEC),
+    INVERT_GRAVITY_PACKET(InvertGravityPacket.class, InvertGravityPacket.PACKET_CODEC),
+    OVERWRITE_GRAVITY_PACKET(OverwriteGravityPacket.class, OverwriteGravityPacket.PACKET_CODEC),
+    UPDATE_GRAVITY_PACKET(UpdateGravityPacket.class, UpdateGravityPacket.PACKET_CODEC)
+    ;
+
+    private final PacketType<?> type;
+
+    <T extends BasePacketPayload> GravityPackets(Class<T> clazz, PacketCodec<? super RegistryByteBuf, T> codec) {
+        String name = this.name().toLowerCase(Locale.ROOT);
+        this.type = new PacketType<>(
+                new CustomPayload.Id<>(GravityChangerMod.id(name)),
+                clazz, codec
+        );
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends CustomPayload> CustomPayload.Id<T> getId() {
+        return (CustomPayload.Id<T>) this.type.type();
+    }
+
+    public static void registerPackets() {
+        for (GravityPackets packet : values()) {
+            packet.type.register();
+        }
+    }
+
+    private record PacketType<T extends BasePacketPayload>(CustomPayload.Id<T> type, Class<T> clazz, PacketCodec<? super RegistryByteBuf, T> codec) {
+        @SuppressWarnings("unchecked")
+        public void register() {
+            boolean clientbound = ClientboundPacketPayload.class.isAssignableFrom(this.clazz);
+            boolean serverbound = ServerboundPacketPayload.class.isAssignableFrom(this.clazz);
+            if (clientbound) {
+                PacketType<ClientboundPacketPayload> casted = (PacketType<ClientboundPacketPayload>) this;
+                PayloadTypeRegistry.playS2C().register(casted.type, casted.codec);
+                ClientPlayNetworking.registerGlobalReceiver(casted.type, ClientboundPacketPayload::handle);
+            }
+            if (serverbound) {
+                PacketType<ServerboundPacketPayload> casted = (PacketType<ServerboundPacketPayload>) this;
+                PayloadTypeRegistry.playC2S().register(casted.type, casted.codec);
+                ServerPlayNetworking.registerGlobalReceiver(casted.type, ServerboundPacketPayload::handle);
+            }
+        }
+    }
+}
