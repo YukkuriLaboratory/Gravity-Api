c455e6d0c553c3f56b53ca42d62b73283472cdba
sysnote8main <sysnote8main@gmail.com>
2024-11-18 23:22:40 +0900

    [Can't-Build] Add BasePacketPayload

---
+++ src/main/java/com/fusionflux/gravity_api/util/packet/BasePacketPayload.java
@@ -0,0 +1,16 @@
+package com.fusionflux.gravity_api.util.packet;
+
+import net.minecraft.network.packet.payload.CustomPayload;
+
+public sealed interface BasePacketPayload extends CustomPayload permits ClientboundPacketPayload, ServerboundPacketPayload {
+    PacketTypeProvider getTypeProvider();
+
+    @Override
+    default CustomPayload.Id<? extends CustomPayload> getId() {
+        return this.getTypeProvider().getId();
+    }
+
+    interface PacketTypeProvider {
+        <T extends CustomPayload> CustomPayload.Id<T> getId();
+    }
+}
