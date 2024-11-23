a2b5f8b4683769e7b4828ac2a5641890002c171d
sysnote8main <sysnote8main@gmail.com>
2024-11-23 11:26:56 +0900

    [Can't-Build] Add Packet ids in Networking.java

--- src/main/java/com/fusionflux/gravity_api/network/Networking.java
+++ src/main/java/com/fusionflux/gravity_api/network/Networking.java
@@ -1,4 +1,12 @@
 package com.fusionflux.gravity_api.network;
 
 public class Networking {
+    // Gravity general packets
+    public static final NetworkId GRAVITY_SYNC = NetworkId.of("gravity_sync");
+    public static final NetworkId GRAVITY_UPDATE = NetworkId.of("gravity_update");
+    public static final NetworkId GRAVITY_INVERT = NetworkId.of("gravity_invert");
+    public static final NetworkId GRAVITY_OVERWRITE = NetworkId.of("gravity_overwrite");
+
+    // Gravity strength packets
+    public static final NetworkId GRAVITY_STRENGTH_SYNC = NetworkId.of("gravity_strength_sync");
 }
