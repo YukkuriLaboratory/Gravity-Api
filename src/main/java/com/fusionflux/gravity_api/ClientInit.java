8a5583a86bf1236173cfe6b0ce48b942eb728f78
sysnote8main <sysnote8main@gmail.com>
2024-11-23 11:31:59 +0900

    [Can't-Build] Add registerPackets in ClientInit temporary

--- src/main/java/com/fusionflux/gravity_api/ClientInit.java
+++ src/main/java/com/fusionflux/gravity_api/ClientInit.java
@@ -1,10 +1,12 @@
 package com.fusionflux.gravity_api;
 
+import com.fusionflux.gravity_api.util.packet.GravityPackets;
 import net.fabricmc.api.ClientModInitializer;
 
 public class ClientInit implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 //        GravityChannel.initClient();
+        GravityPackets.registerPackets();
     }
 }
